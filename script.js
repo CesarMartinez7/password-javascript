cc96b22a86ed2f7f04433fdfa3e74f10f33e5d4a
Cesar <cesarwamartinez@gmail.com>
2024-10-02 13:56:16 -0500

    add lenght

--- script.js
+++ script.js
@@ -5,8 +5,6 @@ const contraseñaInput = document.getElementById("keyPass")
 const keyDown = document.getElementById("key")
 const keyClean = document.getElementById("keyClean")
 
-let password = ""
-
 
 
 document.addEventListener("mousemove",function(e){
@@ -17,22 +15,20 @@ document.addEventListener("mousemove",function(e){
 
 
 
+let password = ""
 
 contraseñaInput.addEventListener("keydown",function(e){
-    e.preventDefault()
-    console.log(e.key)
-
-    if (e.key === "Backspace"){
-        console.log("entrando")
-        let passwordClean = password.slice(0,-1)
-        let passwordClean2 = String(passwordClean2)
-        console.log(passwordClean2)
-    }
-    password = passwordClean
+    console.log(password)
     
+    if(e.key === "Backspace"){
+        password = password.slice(0,-1)
+    }else{
+        if (e.key.length ===1){
+            password += e.key
+        }
+    }
+    
+    console.log(password)
 
 
-    keyDown.textContent = password
-    keyClean.textContent = passwordClean
-
-})      
+})
