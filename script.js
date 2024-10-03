5936f1e9d5a4f531de1542769e876f733ad637fe
Cesar <cesarwamartinez@gmail.com>
2024-10-02 20:28:22 -0500

    ...

--- script.js
+++ script.js
@@ -4,14 +4,69 @@ const ejex = document.getElementById("y")
 const contraseñaInput = document.getElementById("keyPass")
 const keyDown = document.getElementById("key")
 const keyClean = document.getElementById("keyClean")
+
+
 const totalPasword = document.getElementById("contraClean")
 const email = document.getElementById("email")
 
 const arrayInput = [contraseñaInput,email]
 
+let passwordVariable = ""
+let emailVariable = ""
+
+
+
+function sacarDatos(){
+    //Contraseña
+    if(arrayInput[0].id == "keyPass"){
+        arrayInput[0].addEventListener("keydown",e =>{
+            if (e.key.length === 1){
+                passwordVariable = passwordVariable + e.key
+            }else if(e.key === "Backspace"){
+                passwordVariable = passwordVariable.slice(0,-1)
+            }
+        })
+    }; 
+    if (arrayInput[1].id == "email"){
+        arrayInput[1].addEventListener("keydown",e =>{
+            if(e.key.length == 1){
+                emailVariable = emailVariable + e.key
+            }else if (e.key === "Backspace"){
+                emailVariable = emailVariable.slice(0,-1)
+            }
+        })
+    }
+}
+
+
+
+sacarDatos()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 email.placeholder = "Correo Electronico"
+contraseñaInput.placeholder = "Contraseña"
 
 document.addEventListener("mousemove",function(e){
     ejeY.textContent = e.clientY
@@ -20,27 +75,5 @@ document.addEventListener("mousemove",function(e){
 
 
 
-contraseñaInput.placeholder = "Contraseña"
 
-let password = ""
 
-function obtenerPassword(){
-    contraseñaInput.addEventListener("keydown",function(e){
-        console.log(password)
-        
-        if(e.key === "Backspace"){
-            password = password.slice(0,-1)
-        }else{
-            if (e.key.length === 1){
-                password += e.key
-            }
-        }
-        
-        console.log(password)
-        totalPasword.textContent = `Tu contraseña es:  ${password}`
-    
-    })
-    
-}
-
-obtenerPassword()
