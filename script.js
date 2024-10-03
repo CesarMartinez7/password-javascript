f4cad5f57b996eb5a1600c91b01a8d808719be37
Cesar <cesarwamartinez@gmail.com>
2024-10-02 19:08:55 -0500

    better things

--- script.js
+++ script.js
@@ -5,31 +5,42 @@ const contraseñaInput = document.getElementById("keyPass")
 const keyDown = document.getElementById("key")
 const keyClean = document.getElementById("keyClean")
 const totalPasword = document.getElementById("contraClean")
+const email = document.getElementById("email")
+
+const arrayInput = [contraseñaInput,email]
 
 
+
+email.placeholder = "Correo Electronico"
 
 document.addEventListener("mousemove",function(e){
     ejeY.textContent = e.clientY
     ejex.textContent = e.clientX
 })
 
-contraseñaInput.focus() 
 
+
+contraseñaInput.placeholder = "Contraseña"
 
 let password = ""
 
-contraseñaInput.addEventListener("keydown",function(e){
-    console.log(password)
-    
-    if(e.key === "Backspace"){
-        password = password.slice(0,-1)
-    }else{
-        if (e.key.length === 1){
-            password += e.key
+function obtenerPassword(){
+    contraseñaInput.addEventListener("keydown",function(e){
+        console.log(password)
+        
+        if(e.key === "Backspace"){
+            password = password.slice(0,-1)
+        }else{
+            if (e.key.length === 1){
+                password += e.key
+            }
         }
-    }
+        
+        console.log(password)
+        totalPasword.textContent = `Tu contraseña es:  ${password}`
     
-    console.log(password)
-    totalPasword.textContent = `Tu contraseña es :  ${password}`
+    })
+    
+}
 
-})
+obtenerPassword()
